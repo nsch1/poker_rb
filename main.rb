d644a2c29d20ccecdd42ee9755880304ed61abe4
nsch1 <x>
2018-02-17 10:41:27 +0100

    Adds displaying of info and asking for user input

--- main.rb
+++ main.rb
@@ -8,6 +8,10 @@ puts
 
 player = Table.add_player(player_name)
 
+puts "Hello #{player.name}! The cards are being shuffled."
+puts
+
+dealer.shuffle
 dealer.deal_all_cards
 
 puts "You have: #{player.card_info[1]} of #{player.card_info[0]} & #{player.card_info[3]} of #{player.card_info[2]}."
